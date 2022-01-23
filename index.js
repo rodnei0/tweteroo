cae6643d4c0cc6d87c18b3d65bade406460fba55
rodnei0 <rodnei0@gmail.com>
2022-01-23 12:25:36 -0400

    testing loading tweets

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ app.use(express.json());
 let users = [];
 let tweets = [];
 
+users = [   {"username": "rodnei0", 
+             "avatar": "https://img.freepik.com/fotos-gratis/imagem-aproximada-em-tons-de-cinza-de-uma-aguia-careca-americana-em-um-fundo-escuro_181624-31795.jpg?size=626&ext=jpg" },
+            {"username": "rodnei1", 
+            "avatar": "https://img.freepik.com/fotos-gratis/3d-rendem-de-uma-mesa-de-madeira-com-uma-imagem-defocussed-de-um-barco-em-um-lago_1048-3432.jpg?size=626&ext=jpg" },
+            {"username": "rodnei2", 
+            "avatar": "https://teste" }
+
+        ];
+
+tweets = [
+    { "username": "rodnei2", "tweet": "1" },
+    { "username": "rodnei0", "tweet": "1" },
+    { "username": "rodnei0", "tweet": "2" },
+    { "username": "rodnei1", "tweet": "1" },
+    { "username": "rodnei1", "tweet": "2" },
+    { "username": "rodnei2", "tweet": "2" }
+];
+
 app.post("/sign-up", (req, res) => {
     const username = req.body.username;
     const avatar = req.body.avatar;
@@ -26,8 +44,21 @@ app.post("/tweets", (req, res) => {
     res.send("OK")
 });
 
-// app.get("/tweets", (req, res) => {
-    
-// });
+app.get("/tweets", (req, res) => {
+    let filteredTweets = [];
+    let avatars = [];
+    let teste = [];
+
+    for (let i = tweets.length-1;i >= tweets.length-10; i--){
+        if (i < 0) {
+            break
+        }
+        avatars = users.find(user => user.username === tweets[i].username);
+        filteredTweets.push(tweets[i]);
+        filteredTweets[filteredTweets.length-1].avatar = avatars.avatar; 
+    }
+
+    res.send(filteredTweets);
+});
 
 app.listen(5000);
